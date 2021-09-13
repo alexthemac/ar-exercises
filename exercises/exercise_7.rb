b65fc1695eba9979b3e05a49095b125239ea30be
Alex MacLure <almaclure@gmail.com>
2021-09-13 23:13:25 +0000

    Finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Enter new store name
+puts 'Enter new store name:'
+@store_name = gets.chomp
+store = Store.create(name: @store_name, annual_revenue: 1000)
+
+# Expect error to show if name entered is too short (< 3 characters)
+# due to validations put into the lib->store.rb
+puts store.errors[:name]
+
+# Expect error to always show as we are not setting
+# annual_revenue anywhere for the new store
+puts store.errors[:annual_revenue]
