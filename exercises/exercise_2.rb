a813f156e10cd1eec549453ac49a5d5033607356
Alex MacLure <almaclure@gmail.com>
2021-09-13 21:27:42 +0000

    Exercise 1 and 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Create instance variables representing store 1 and store 2
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# Update the store column for @store1
+@store1.update(name: "Alex's store")
